c368d499b012801d55ca23f0bd8fe3394ce7551b
Viktor (Ikon) VAD <vad.viktor+github@gmail.com>
2022-06-19 21:09:57 +0100

    :building_construction: Use IntelliJ's build system.

--- ___build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-GOOS=linux GOARCH=amd64 go build -a -race -buildvcs=false -o ./build/ddns-amd64
-GOOS=linux GOARCH=arm64 GOARM=7 go build -a -buildvcs=false -o ./build/ddns-arm64v7
